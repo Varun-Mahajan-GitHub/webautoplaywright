141969da91d04b6f2512163be789379abd795fdd
Varun Mahajan <varun.mahajan@saavn.com>
2024-06-21 16:34:08 +0530

    First commit

--- tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.jiosaavn.com/');
-  await expect(page.getByRole('heading', { name: 'Recommended Artist Stations' })).toBeVisible();
-});
--- tests/test-2.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.jiosaavn.com/');
-  await expect(page.getByRole('heading', { name: 'Radio Stations' })).toBeVisible();
-  await page.locator('article').filter({ hasText: 'Bollywood Retro 70s-80sHindi' }).locator('i').click({
-    button: 'right'
-  });
-});
